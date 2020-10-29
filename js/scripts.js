1dd6f83f2e7eb325019f235944f1fd2a1141e03a
alizavr <alina.dmitrievna2407@gmail.com>
2020-10-29 22:39:09 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:true,
+      navText: [ '', ' ' ],
+
+      responsive:{
+          0:{
+              items:1
+          },
+
+          1000:{
+              items:1
+          }
+      }
+  });
+});
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
